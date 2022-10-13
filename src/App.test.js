1ec3fe35762b037a3d9c13619b33d99fe93c5a7a
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-13 09:27:13 +0530

    Initialized CRA.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Learn React Hooks', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Learn React Hooks/i);
   expect(linkElement).toBeInTheDocument();
 });
