3a457571c69eec41c7e8261f809b35678137169e
Ajeet jha <jhaajeetcs@gmail.com>
2022-10-13 10:34:05 +0530

    Husky added.

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders Learn React Hooks', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Learn React Hooks/i);
-  expect(linkElement).toBeInTheDocument();
+	render(<App />);
+	const linkElement = screen.getByText(/Learn React hooks/i);
+	expect(linkElement).toBeInTheDocument();
 });
